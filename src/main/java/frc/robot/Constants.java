89eaad6ac4ea7d36913d17d57c2a1bdd3ae0fa13
James White <Grimkey@gmail.com>
2023-01-07 15:55:57 -0600

    Adhering to recommendations for command code.

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static final int Degrees0 = 0;
+  public static final int Degrees45 = 45;
+  public static final int Degrees90 = 90;
+  public static final int Degrees180 = 180;
+  public static final int Degrees270 = 270;
+
+  public static final int TeamNumber = 6377;
+
+  public static final double MetersToInches = 39.3701;
+  /**
+   * TalonFX number of ticks per 1 motor revolution
+   *
+   * @link https://github.com/CrossTheRoadElec/Phoenix-Documentation#what-are-the-units-of-my-sensor
+   */
+  public static final int TalonFXTicksPerRevolution = 2048;
+
+  public static final int TalonFullPowerInternal = 1023;
+
+  /**
+   * Pigeon number of units per 1 full 360 degree yaw rotation
+   *
+   * @link https://github.com/CrossTheRoadElec/Phoenix-Documentation#what-are-the-units-of-my-sensor
+   */
+  public static final int PigeonUnitsPerRotation = 8192;
+
+  /** Default timeout to use when sending messages over the can bus */
+  public static final int CANTimeout = 50;
+
+  /**
+   * You can have up to 2 devices assigned remotely to a talon/victor HowdyBot conventions: -
+   * Drivetrains: - use 0 for a remote left encoder - use 1 for a remote pigeon
+   */
+  public static final int RemoteEncoder = 0;
+
+  public static final int RemotePigeon = 1;
+
+  /**
+   * The talon runs 2 pid loops, a primary and an aux pid loop. HowdyBot conventions: - Drivetrains:
+   * - use primary for closed loop velocity or closed loop position - use aux for turning
+   */
+  public static final int PIDLoopPrimary = 0;
+
+  public static final int PIDLoopTurn = 1;
+
+  /** HowydBot convention: the aux pid is expressed in units of 1/10 of a degree */
+  public static final int TurnPIDRange = 3600;
+
+  /**
+   * The talon can have up to 4 pid loops configured. HowdyBot conventions: - Drivetrains: - Slot0:
+   * velocity (primary pid loop) - Slot1: turninig (on aux pid loop) - Slot2: position (primary pid
+   * loop) - Slot3: motion profile mode (primary pid loop)
+   */
+  public static final int PIDSlotVelocity = 0;
+
+  public static final int PIDSlotTurning = 1;
+  public static final int PIDSlotPosition = 2;
+  public static final int PIDSlotMotionProfile = 3;
+
+  public static final double doubleEpsilon = 0.001;
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystem.DeploySubsystem;
+import frc.robot.subsystems.DeploySubsystem;
 
 public class RobotContainer {
   private final DeploySubsystem deploySubsystem = new DeploySubsystem();
