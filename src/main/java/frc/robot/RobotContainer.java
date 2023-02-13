a808501000102d22d12ba9f148c30ba1b0449e77
tylerhedge <34404515+tylerhedge@users.noreply.github.com>
2023-02-12 19:04:11 -0600

    added emilies stupid button (#39)
    * added emilies stupid button
    
    * Spotless!!
    
    ---------
    
    Co-authored-by: Dave Lewis <dave.lewis@atxrobotics.org>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,12 +5,15 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.BooleanTopic;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.DefaultDriveCommand;
@@ -78,10 +81,19 @@ public class RobotContainer {
     Trigger driverMidButton = driver.x();
     Trigger driverLowButton = driver.y();
     Trigger driverGoButton = driver.b();
+    Trigger driverResetFieldNorth = driver.start();
     Trigger driverToggleGamePieceButton = driver.leftBumper();
 
     intakeButton.whileTrue(
         Commands.startEnd(() -> endAffector.intake(), () -> endAffector.idle(), endAffector));
+
+    driverResetFieldNorth.whileTrue(
+        new InstantCommand(
+            () ->
+                fieldPositioningSystem.resetRobotPosition(
+                    new Pose2d(
+                        fieldPositioningSystem.getRobotXYPose().getTranslation(),
+                        new Rotation2d(Math.PI)))));
 
     // This watches for the buttons to be pressed then released,
     // thereby making the arm extend quickly.
