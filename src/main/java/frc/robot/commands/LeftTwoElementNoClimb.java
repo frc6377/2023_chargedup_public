d8e3ecb79758bba5baa4a6fd73b5ac922cd6c2f6
Howdy Bots <thebots@howdybots.org>
2023-03-17 13:22:40 -0500

    Updates

--- src/main/java/frc/robot/commands/LeftTwoElementNoClimb.java
+++ src/main/java/frc/robot/commands/LeftTwoElementNoClimb.java
@@ -43,7 +43,7 @@ public class LeftTwoElementNoClimb extends SequentialCommandGroup {
         new ScheduleCommand(
             new WaitCommand(0.5)
                 .andThen(
-                    new ArmPowerCommand(Constants.LOW_CUBE_ARM_POSITION, arm, 3).withTimeout(0.5))),
+                    new ArmPowerCommand(Constants.HIGH_STOWED_ARM_POSITION, arm, 3).withTimeout(0.5))),
         factory.generateCommandFromFile("PickSecondElementBlue", false));
   }
 }
