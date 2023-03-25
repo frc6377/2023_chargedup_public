744afbd09d8dd2e8f7f5f79fa37a8d5613880e73
tylerhedge <34404515+tylerhedge@users.noreply.github.com>
2023-03-25 10:55:27 -0500

    Fixed Recursive Loop

--- src/main/java/frc/robot/networktables/DeltaBoard.java
+++ src/main/java/frc/robot/networktables/DeltaBoard.java
@@ -2,20 +2,22 @@ package frc.robot.networktables;
 
 import java.util.Hashtable;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class DeltaBoard {
   private static Hashtable<String, String> stringTable = new Hashtable<>();
   private static Hashtable<String, Double> doubleTable = new Hashtable<>();
 
   public static void putNumber(String key, Double val) {
     if (!doubleTable.containsKey(key) || !doubleTable.get(key).equals(val)) {
-      DeltaBoard.putNumber(key, val);
+      SmartDashboard.putNumber(key, val);
       doubleTable.put(key, val);
     }
   }
 
   public static void putString(String key, String val) {
     if (!stringTable.containsKey(key) || !stringTable.get(key).equals(val)) {
-      DeltaBoard.putString(key, val);
+      SmartDashboard.putString(key, val);
       stringTable.put(key, val);
     }
   }
