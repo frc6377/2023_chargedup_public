c6b0eaa0d0199df9e4ed80508579bb3b22d8e011
Howdy Bots <thebots@howdybots.org>
2023-04-02 14:02:39 -0500

    volume auto left tuned

---
+++ src/main/java/frc/robot/commands/LeftVolumeAuto.java
@@ -0,0 +1,20 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drivetrain.DrivetrainSubsystem;
+
+public class LeftVolumeAuto extends SequentialCommandGroup {
+    public LeftVolumeAuto (DrivetrainSubsystem drive, SwerveAutoFactory factory){
+        addCommands(
+            factory.generateCommandFromFile("LeftVolumeFirstElement", true),
+            factory.generateCommandFromFile("LeftVolumeScoreFirst", false),
+            factory.generateCommandFromFile("LeftVolumeSecondElement", false),
+            factory.generateCommandFromFile("LeftVolumeScoreSecond", false),
+            factory.generateCommandFromFile("LeftVolumeThirdElement", false),
+            factory.generateCommandFromFile("LeftVolumeScoreThird", false),
+            factory.generateCommandFromFile("LeftVolumeFourthElement", false),
+            factory.generateCommandFromFile("LeftVolumeScoreFourth", false)
+
+        );
+    }
+}
