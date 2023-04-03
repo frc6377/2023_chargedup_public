97fe025b999db69b761660b6df727094f04f7163
Howdy Bots <thebots@howdybots.org>
2023-04-02 20:03:44 -0500

    left side about perfect

--- src/main/java/frc/robot/commands/LeftTwoElement.java
+++ src/main/java/frc/robot/commands/LeftTwoElement.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import java.time.Instant;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -19,7 +21,9 @@ public class LeftTwoElement extends SequentialCommandGroup {
       EndAffectorSubsystem endAffector) {
     Command unbind = new ArmPowerCommand(arm::getUnbindPosition, arm, 3);
     addCommands(
-        new ScheduleCommand(unbind),
+        new InstantCommand(()-> endAffector.halt()),
+        new ArmPowerCommand(Constants.BACKWARDS_HIGH_CONE, arm, 3),
+        new WaitCommand(1),
         new InstantCommand(
             () -> {
               endAffector.setCube();
@@ -28,27 +32,19 @@ public class LeftTwoElement extends SequentialCommandGroup {
             () -> {
               endAffector.intake();
             }),
-        new ScheduleCommand(
-            new ArmPowerCommand(Constants.LOW_CUBE_ARM_POSITION, arm, 3)).alongWith(
-        factory.generateCommandFromFile("PickFirstElementBlue", true)),
-        new ScheduleCommand(
-            new ArmPowerCommand(Constants.HIGH_STOWED_ARM_POSITION, arm, 3).withTimeout(0.5)),
-        new ScheduleCommand(
-            new WaitCommand(2)
-                .andThen(
-                    new ArmPowerCommand(Constants.HIGH_CUBE_ARM_POSITION, arm, 3)
-                        .withTimeout(1.5))),
-        new InstantCommand(() -> endAffector.idle()),
-        factory.generateCommandFromFile("ScoreFirstElementBlue", false),
+            new WaitCommand(0.25),
+            new ArmPowerCommand(Constants.LOW_CUBE_ARM_POSITION, arm, 3).alongWith(
+        new WaitCommand(0.5).andThen(factory.generateCommandFromFile("PickFirstElementBlue", true).andThen(new InstantCommand(() -> endAffector.idle())))),
+            new ArmPowerCommand(Constants.HIGH_STOWED_ARM_POSITION, arm, 3)
+            .andThen(new WaitCommand(1)
+            .andThen(new ArmPowerCommand(Constants.HIGH_CUBE_ARM_POSITION, arm, 3)
+            )).alongWith(factory.generateCommandFromFile("ScoreFirstElementBlue", false)),
+    
         new WaitCommand(0.75),
         new InstantCommand(() -> endAffector.fastOutake()),
         new WaitCommand(0.5),
-        new ScheduleCommand(
-            new WaitCommand(0.5)
-                .andThen(
-                    new ArmPowerCommand(Constants.STOWED_ARM_POSITION, arm, 3).withTimeout(0.5))),
         new InstantCommand(() -> endAffector.idle()),
-        factory.generateCommandFromFile("ClimbBlue", false),
+        factory.generateCommandFromFile("ClimbBlue", false).alongWith(new WaitCommand(0.5).andThen(new ArmPowerCommand(Constants.STOWED_ARM_POSITION, arm, 3))),
         new AutoBalanceCommand(drive));
   }
 }
