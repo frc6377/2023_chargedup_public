8bfe14d533e4dec759cb10c1b05c0ad12d4b741c
tylerhedge <34404515+tylerhedge@users.noreply.github.com>
2023-04-15 12:59:02 -0500

    right side mostly good

--- src/main/java/frc/robot/commands/autos/RightThreeElement.java
+++ src/main/java/frc/robot/commands/autos/RightThreeElement.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.autos;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -17,8 +18,9 @@ public class RightThreeElement extends SequentialCommandGroup {
       ArmSubsystem arm,
       EndAffectorSubsystem endAffector) {
     addCommands(
+        Commands.print("Right Three Element Running"),
         new InstantCommand(() -> endAffector.halt()),
-        new ArmPowerCommand(Constants.BACKWARDS_HIGH_CONE, arm, 3),
+        new ArmPowerCommand(Constants.BACKWARDS_MID_CONE, arm, 3),
         new WaitCommand(0.25),
         new InstantCommand(
             () -> {
@@ -34,32 +36,26 @@ public class RightThreeElement extends SequentialCommandGroup {
                 new WaitCommand(0.5)
                     .andThen(
                         factory
-                            .generateCommandFromFile("PickFirstElementRight", true, 3, 2.5)
+                            .generateCommandFromFile("PickFirstElementRight", true, 4, 3)
                             .andThen(new InstantCommand(() -> endAffector.idle())))),
         new ArmPowerCommand(Constants.HIGH_STOWED_ARM_POSITION, arm, 3)
             .andThen(
-                new WaitCommand(1)
-                    .andThen(new ArmPowerCommand(Constants.HIGH_CUBE_ARM_POSITION, arm, 3)))
-            .alongWith(factory.generateCommandFromFile("ScoreFirstElementRight", false, 3, 2.5)),
-        new WaitCommand(0.25),
+                new WaitCommand(0.5)
+                    .andThen(new ArmPowerCommand(Constants.MID_CUBE_ARM_POSITION, arm, 3)))
+            .alongWith(factory.generateCommandFromFile("ScoreFirstElementRight", false, 4, 2.9)),
         new InstantCommand(() -> endAffector.fastOutake()),
         new WaitCommand(0.25),
         new InstantCommand(() -> endAffector.halt()),
-        new ArmPowerCommand(Constants.HIGH_STOWED_ARM_POSITION, arm, 3)
-            .alongWith(new WaitCommand(0.25)
+        new ArmPowerCommand(Constants.HIGH_STOWED_ARM_POSITION, arm, 3).andThen(new WaitCommand(0.0)
+        .andThen(new ArmPowerCommand(Constants.LOW_CUBE_ARM_POSITION, arm, 3)))
+            .alongWith(new WaitCommand(0.0)
                 .andThen(factory.generateCommandFromFile("PickSecondElementRight", true, 3, 2.5))) //yes this should be true
-            .alongWith(new WaitCommand(2)
-                .andThen(new ArmPowerCommand(Constants.LOW_CUBE_ARM_POSITION, arm, 3)))
+            
             .alongWith(new WaitCommand(0.25)
                 .andThen(new InstantCommand(()-> endAffector.intake()))),
         new ArmPowerCommand(Constants.HIGH_STOWED_ARM_POSITION, arm, 3)
-            .andThen(new WaitCommand(2)
-                .andThen(new ArmPowerCommand(Constants.MID_CUBE_ARM_POSITION, arm, 3)))
-            .alongWith(factory.generateCommandFromFile("ScoreSecondElementRight", false, 3, 2)),
-            new WaitCommand(0.25),
-            new InstantCommand(()-> endAffector.fastOutake()),
-            new WaitCommand(0.25),
-            new InstantCommand(()-> endAffector.halt()),
-            new ArmPowerCommand(Constants.HIGH_STOWED_ARM_POSITION, arm, 3));
+            .andThen(new WaitCommand(0.25).andThen(new InstantCommand(()-> endAffector.idle()))
+                .andThen(new ArmPowerCommand(Constants.HYBRID_CUBE_ARM_POSITION, arm, 3)).andThen(new InstantCommand(()-> endAffector.fastOutake())))
+            .alongWith(factory.generateCommandFromFile("ScoreSecondElementRight", false, 3, 3)));
   }
 }
