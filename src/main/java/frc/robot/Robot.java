4f42ee4cb0730b2ffe3f347314b7555dda8695f8
Jackson <Jacksona@howdybots.org>
2023-09-27 20:01:14 -0500

    Log DriverStation Controllers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -23,6 +25,10 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+
+    DataLogManager.start();
+    // Automatically log joystick and Driver Station control data
+    DriverStation.startDataLog(DataLogManager.getLog());
   }
 
   @Override
